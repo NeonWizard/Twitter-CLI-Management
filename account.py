0624e7ae3112d981de87d441d976022554aa11e5
NeonWizard <averagewizard13@gmail.com>
2017-04-18 13:17:56 -0600

    FULLY FUNCTIONING + TRACKS PEOPLE WHO HAVE BEEN FOLLOWED

--- account.py
+++ account.py
@@ -3,49 +3,60 @@ import random
 import time
 
 class Account:
-	def __init__(self, id):
-		self.API = Twython("U8NXVVgoLFmCINZqMiE7Fhi9G", "27sx2V6j2FSIW2kMpDu6OrEe1NfhRYbbUXmVMWRlAMErJR1jWD", "850089475128438784-k1JMEgPaTaIyTrE1hpQwyJraXWQVWLw", "8wZNArpbRHcyHHGziYSJxYYfPOMdcUrKCcZPMpXXVfX2O")
+	def __init__(self, id, keys):
+		self.API = Twython(**keys)
 		self.screenName = self.API.verify_credentials()["screen_name"]
 		self.id = id
 
-		self.hasFollowed = set()									# People that HAVE been followed by this account but aren't currently
-		self.isFollowed = set(self.API.get_friends_ids()["ids"])	# People that ARE being followed by this account currently
-
+		self.loadFollowed()											# The followed set includes people that have been followed OR are currently being followed
+		self.isFollowed = set(self.API.get_friends_ids()["ids"])	# The isFollowed set ONLY includes people currently being followed
 		self.followers = set(self.API.get_followers_ids()["ids"])	# People following this account
 
-	def dumpHasFollowed(self):
-		print("Dumping hasFollowed... ", end="")
-		with open("hasfollowed.txt", 'w') as openFile:
-			for ID in self.hasFollowed:
-				openFile.write(str(ID) + "\n")
-		print("done.")
+	# def dumpFollowed(self):
+	# 	print("Dumping followed... ", end="")
+	# 	with open("hasfollowed.txt", 'w') as openFile:
+	# 		for ID in self.followed:
+	# 			openFile.write(str(ID) + "\n")
+	# 	print("done.")
 
-	def loadHasFollowed(self):
-		print("Loading hasFollowed... ", end="")
+	def writeFollowed(self, ID):
+		with open("hasfollowed.txt", 'a') as openFile:
+			openFile.write(str(ID) + "\n")
+
+	def loadFollowed(self):
+		self.followed = set()
+
+		print("Loading followed... ", end="")
 		with open("hasfollowed.txt", 'r') as openFile:
 			for line in openFile:
 				line = line.rstrip("\n")
-				self.hasFollowed.add(int(line))
+				self.followed.add(int(line))
 		print("done.")
 
-	def updateFollowers(self):
-		self.followers = self.API.get_followers_ids()["ids"]
+	def follow(self, ID):
+		try:
+			self.API.create_friendship(user_id=ID)
+			print("Followed {}!".format(ID))
+
+			self.followed.add(ID)
+			self.writeFollowed(ID)
+
+			return True
+		except TwythonError as e:
+			# This is mostly just for users I've already requested to follow but haven't accepted yet
+			# print(e)
+			return False
 
 	def followAllOf(self, target_id, amount=1000):
 		amount = min(amount, 1000)
 		count = 0
 		for friend_id in self.API.get_followers_ids(user_id=target_id)["ids"]:
-			if friend_id == self.id: 			continue	# dat me
-			if friend_id in self.isFollowed: 	continue 	# already following :0
-			if friend_id in self.hasFollowed: 	continue	# already followed but no follow back :< 
+			if friend_id == self.id: 		continue	# dat me
+			if friend_id in self.followed: 	continue 	# already followed/following
 
 			# Create friendship
-			try:
-				self.API.create_friendship(user_id=friend_id)
-				print("Added friend {}!".format(friend_id))
-				self.isFollowed.append(friend_id)
-			except TwythonError:
-				# This is mostly just for users I've already requested to follow but haven't accepted yet
+			if not self.follow(friend_id):
+				# Couldn't add them >:y
 				continue
 
 			# Make a tiny bit of a delay so it doesn't flood the Twitter servers
@@ -57,37 +68,33 @@ class Account:
 				break
 
 	def unfollowNonFollowers(self):
-		tmp = set()
+		unfollowed = set()
 		for followed_id in self.isFollowed:
 			if followed_id not in self.followers:
 				self.API.destroy_friendship(user_id=followed_id)
-				tmp.add(followed_id)
-				self.hasFollowed.add(followed_id)
-				print("Unfollowed {} cuz he/she wasn't following me back >:c".format(user_id))
+				unfollowed.add(followed_id)
+				print("Unfollowed {} cuz he/she wasn't following me back >:c".format(followed_id))
 				break
 
-		self.isFollowed = self.isFollowed - tmp
+		self.isFollowed = self.isFollowed - unfollowed
 
 	def followBackAll(self):
 		for follower_id in self.followers:
 			if follower_id not in self.isFollowed:
-				try:
-					self.API.create_friendship(user_id=follower_id)
-					print("Followed back {}!".format(follower_id))
-				except TwythonError:
-					continue
+				self.follow(follower_id)
 
 
 def main():
-	spooky = Account(id=850089475128438784)
+	spooky = Account(id=850089475128438784, keys={
+		"app_key": "U8NXVVgoLFmCINZqMiE7Fhi9G",
+		"app_secret": "27sx2V6j2FSIW2kMpDu6OrEe1NfhRYbbUXmVMWRlAMErJR1jWD",
+		"oauth_token": "850089475128438784-k1JMEgPaTaIyTrE1hpQwyJraXWQVWLw",
+		"oauth_token_secret": "8wZNArpbRHcyHHGziYSJxYYfPOMdcUrKCcZPMpXXVfX2O"
+	})
 
-	spooky.loadHasFollowed()
-
-	# spooky.followAllOf(target_id=2432753774, amount=2)	# Follow a few of Telepathic's followers
-	# spooky.followBackAll()
-	spooky.unfollowNonFollowers()
-
-	spooky.dumpHasFollowed()
+	spooky.followAllOf(target_id=2432753774, amount=2)	# Follow a few of Telepathics' followers
+	spooky.followBackAll()
+	# spooky.unfollowNonFollowers()
 
 if __name__ == "__main__":
 	main()
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from account import Account
 
+def promptUserAuth():
+	ID = int(input("Please enter your Twitter ID: "))
+	
+	keys = {
+		"app_key": input("Please enter your app key: "),
+		"app_secret": input("Please enter your app secret: "),
+		"oauth_token": input("Please enter your oauth token: "),
+		"oauth_token_secret": input("Please enter your oauth token secret: ")
+	}
+
+	return ID, keys
+
 def main():
-	acc = Account(id=int(input("Please enter your Twitter ID: ")))
+	ID, keys = promptUserAuth()
+	acc = Account(id=ID, keys=keys)
+
 	print("Hello @{}!".format(acc.screenName))
 
+	print("\n" * 50)
 	looping = True
 	while looping:
-		print()
+		print(chr(27) + "[2J")
+		print("==========================")
 		print("What would you like to do?")
+		print("==========================")
 		print()
 		print("1 - Follow back all followers.")
 		print("2 - Follow [x] of [target]'s followers.")
@@ -18,7 +35,11 @@ def main():
 		if option == 1:
 			acc.followBackAll()
 		elif option == 2:
-			print("WIP")
+			print()
+			print(chr(27) + "[2J")
+			ID = int(input("Enter target account's ID: "))
+			amount = int(input("Enter amount to follow: "))
+			acc.followAllOf(ID, amount)
 		elif option == 3:
 			acc.unfollowNonFollowers()
 		elif option == 4:
